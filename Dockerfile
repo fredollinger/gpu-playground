1921644407ec0569cfea1e4bf99292872b56b12f
Frederick.Ollinger <follinge@gmail.com>
2023-03-13 22:17:48 +0000

    HelloWorld Expand Functions
    Add git and vim to Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM nvidia/cuda:12.1.0-devel-ubuntu18.04
+
+RUN apt update \ 
+    && apt -u -qy dist-upgrade \
+    && apt install -qy git vim
--- src/hello.cu
+++ src/hello.cu
@@ -1,15 +1,25 @@
+// Adapted from here: https://www.amazon.com/CUDA-Application-Design-Development-Farber/dp/0123884268
+// Example 1.7
+
 #include <iostream>
 
 using namespace std;
 
 #include <thrust/reduce.h>
-/*
-#include  
-#include 
-#include 
-#include 
-#include 
+#include  <thrust/sequence.h>
+#include  <thrust/host_vector.h>
+#include  <thrust/device_vector.h>
 
-*/
+__global__ void fillKernel(int *a, int n) {
+ int tid = blockIdx.x * blockDim.x + threadIdx.x;
+ if (tid < n) a[tid] = tid;
+}
+
+int fill(int *d_a, int n) {
+    int nThreadsPerBlock = 12;
+    int nBlocks = n/nThreadsPerBlock + ((n%nThreadsPerBlock) ? 1 : 0);
+    fillKernel <<< nBlocks, nThreadsPerBlock >>> (d_a, n);
+}
+
 
 int main() {}
